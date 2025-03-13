788156b9e2c1ef2608edd6dc785188b27a9aca41
Cooper <Cooperransom08@gmail.com>
2025-03-13 19:23:59 +0000

    Polish and test!

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,5 @@ import { searchSubtitles, parseToVTT } from "./funcs";
 // Named exports 😏
 export { searchSubtitles, parseToVTT };
 export type { SubtitleData, SearchSubtitlesParams, QueryParams };
+
+// You can test code here and see what it logs in the console of ur browser.
